64ad991dadaa6e17901db1da6463802f8d275a3b
GwendolyneFreddy <gwendolyne.freddy@gmail.com>
2019-10-09 08:54:05 +0200

    Upload files

---
+++ bolsa/dlg/mhbolsa.d
@@ -0,0 +1,11 @@
+BEGIN ~mhbolsa~
+
+IF ~NumTimesTalkedTo(0)~ THEN BEGIN hola
+   SAY @20 
+   IF ~~ THEN DO ~StartStore("mhbolsa",LastTalkedToBy())~ EXIT
+END
+
+IF ~NumTimesTalkedToGT(0)~ THEN BEGIN bolsa2
+   SAY @21
+   IF ~~ THEN DO ~StartStore("mhbolsa",LastTalkedToBy())~ EXIT
+END
